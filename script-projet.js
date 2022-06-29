b67a8d4e2f3cd26b5dff31fa1ee372768188a3e9
bast478 <bast0ch@protonmail.com>
2022-06-29 16:57:17 +0200

    Travail sur la gallerie desktop

--- script-projet.js
+++ script-projet.js
@@ -4,12 +4,24 @@ function setAttributes(el, attrs) {
     }
 }
 
-// Création des éléments
+// CREATION DES VARIABLES ET ELEMENTS
 
-const leftArrowGallery = document.createElement('SVG');
+const svgNS = "http://www.w3.org/2000/svg";
+
+//***************************Gallerie Desktop**************************
+const listMinPictures = document.querySelectorAll('#main_Gallery > div:not(#grid_main-item) > img'); //Miniatures
+const mainPicture = document.querySelector('#grid_main-item > img');
+const arrow = document.querySelector('#grid_main-item > svg');
+const modal = document.querySelector('#modal');
+
+//------Modal dans la gallerie--------
+const divGridModal = document.createElement('DIV');
+
+//Flèche gauche gallerie
+const leftArrowGallery = document.createElementNS(svgNS, "svg");
 setAttributes(leftArrowGallery, {"width": "67", "height": "154", "viewBox": "0 0 67 154", "fill": "none", "xmlns": "http://www.w3.org/2000/svg"});
 for (let i = 0; i < 2; i++) {
-    let leftArrowGalleryLine = document.createElement('LINE');
+    let leftArrowGalleryLine = document.createElementNS(svgNS, "line");
     if (i === 0) {
         setAttributes(leftArrowGalleryLine, {"x1": "66.0449", "y1": "0.642788", "x2": "1.76609", "y2": "77.2472", "stroke": "white", "stroke-width": "2"});
     } else if (i === 1) {
@@ -17,19 +29,45 @@ for (let i = 0; i < 2; i++) {
     }
     leftArrowGallery.appendChild(leftArrowGalleryLine);
 }
-console.log(leftArrowGallery);
 
+//Flèche droite gallerie
+const rightArrowGallery = document.createElementNS(svgNS, "svg");
+setAttributes(rightArrowGallery, {"width": "67", "height": "154", "viewBox": "0 0 67 154", "fill": "none", "xmlns": "http://www.w3.org/2000/svg"});
+for (let i = 0; i < 2; i++) {
+    let rightArrowGalleryLine = document.createElementNS(svgNS, "line");
+    if (i === 0) {
+        setAttributes(rightArrowGalleryLine, {"x1": "65.5159", "y1": "77.2473", "x2": "1.23717", "y2": "0.642836", "stroke": "white", "stroke-width": "2"});
+    } else if (i === 1) {
+        setAttributes(rightArrowGalleryLine, {"x1": "66.048", "y1": "76.6425", "x2": "1.76605", "y2": "153.251", "stroke": "white", "stroke-width": "2"});
+    }
+    rightArrowGallery.appendChild(rightArrowGalleryLine);
+}
+
+//Croix gallerie
+const crossGallery = document.createElementNS(svgNS, "svg");
+setAttributes(crossGallery, {"width": "39", "height": "38", "viewBox": "0 0 39 38", "fill": "none", "xmlns": "http://www.w3.org/2000/svg"});
+for (let i = 0; i < 2; i++) {
+    let crossGalleryLine = document.createElementNS(svgNS, "line");
+    if (i === 0) {
+        setAttributes(crossGalleryLine, {"x1": "1.29289", "y1": "36.6481", "x2": "36.6482", "y2": "1.29278", "stroke": "white", "stroke-width": "2"});
+    } else if (i === 1) {
+        setAttributes(crossGalleryLine, {"x1": "2.70711", "y1": "1.29289", "x2": "38.0624", "y2": "36.6482", "stroke": "white", "stroke-width": "2"});
+    }
+    crossGallery.appendChild(crossGalleryLine);
+}
+
+//Photo du diapo
 const mainPictureGallery = document.createElement('IMG');
 mainPictureGallery.setAttribute("alt", "Une photo du gouffre.");
 
-// Gallerie Desktop
+//Mettre les éléments du diapo dans une div
+divGridModal.appendChild(leftArrowGallery);
+divGridModal.appendChild(mainPictureGallery);
+divGridModal.appendChild(rightArrowGallery);
+divGridModal.appendChild(crossGallery);
+modal.appendChild(divGridModal);
+// EVENEMENTS
 
-const listMinPictures = document.querySelectorAll('#main_Gallery > div:not(#grid_main-item) > img'); //Miniatures
-const mainPicture = document.querySelector('#grid_main-item > img');
-const arrow = document.querySelector('#grid_main-item > svg');
-const modal = document.querySelector('#modal');
-
-// Evenements
 // Changer de photo au click sur Desktop
 
 listMinPictures.forEach(function(minPicture){
@@ -42,9 +80,7 @@ listMinPictures.forEach(function(minPicture){
 
 // Agrandir la photo au click de la flèche
 
-arrow.addEventListener('click', function(){;
-    modal.appendChild(mainPictureGallery);
+arrow.addEventListener('click', function(){
     mainPictureGallery.src = mainPicture.src;
-    modal.style.display = 'block';
-    modal.appendChild(leftArrowGallery);
+    modal.style.display = "block";
 });
